3779bd876bdd7375819edf60de5fe29c8b89cecd
JJ <860024987@wwcc.edu>
2018-05-09 16:03:04 -0700

    Branch list_vs_head -

--- src/Queue.java
+++ src/Queue.java
@@ -5,37 +5,38 @@ public class Queue<T> extends LinkedList<T> {
     //Since I am now extending LL class, This is duplicating of head in LL.
     //"extends" did that for me.
     //todo: this will be commented out as "list" will be replaced with "head"
-    private LinkedList<T> list;
+//    private LinkedList<T> list;
 
     public Queue()
     {
-        this.list = null;
+        this.head = null;
         this.length = 0;
     }
 
-    public Queue(LinkedList<T> list)
+    public Queue(Node<T> list)
     {
-        this.list = list;
-        this.length = list.length();
+        this.head = list;
+        this.length = length();
     }
 
     public T dequeue()
     {
         --length;
-        return list.removeFirst();
+        return removeFirst();
     }
 
     public void enqueue(T value)
     {
-        this.list.append(value);
+        this.append(value);
         ++length;
     }
 
-    public T peek()
-    {
-        return list.peek();
-    }
-
+    // Since extending from LL it already has this method.
+//    public T peek()
+//    {
+//        return peek();
+//    }
+//
     public int size()
     {
         return length;
