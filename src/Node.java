8b972e960ec96c6480106b9217f6970570785ced
JJ <860024987@wwcc.edu>
2018-05-10 15:58:12 -0700

    Added(changed) fluent interface to Node class. Added more setters for everything in Node.

--- src/Node.java
+++ src/Node.java
@@ -10,7 +10,8 @@
  */
 @SuppressWarnings("WeakerAccess")
 public class Node<T> {
-    private Node<T> next;
+    private Node<T> left;
+    private Node<T> right;
     private T value;
 
     /**
@@ -18,7 +19,9 @@ public class Node<T> {
      */
     public Node()
     {
-        this(null, null);
+        this.value = null;
+        this.left = null;
+        this.right = null;
     }
 
     /**
@@ -30,41 +33,124 @@ public class Node<T> {
     public Node(T value)
     {
         this.value = value;
-        this.next = null;
-        // this(value, null);
+        this.left = null;
+        this.right = null;
     }
 
     /**
-     * Optional all value constructor.
+     * Full constructor with Nodes.
+     *
+     * @param value
+     * @param left
+     * @param right
+     */
+    public Node(T value, Node<T> left, Node<T> right)
+    {
+        this.value = value;
+        this.left = left;
+        this.right = right;
+    }
+
+    /**
+     * Full constructor with values.
+     *
+     * @param value
+     * @param left
+     * @param right
+     */
+    public Node(T value, T left, T right)
+    {
+        this.value = value;
+        this.left = new Node<>(left);
+        this.right = new Node<>(right);
+    }
+
+    /**
+     * Optional value and left constructor.
+     *
+     * If you need this one probably should add one with right as well.
      *
      * @param value
      *     <T>
-     * @param next
+     * @param left
      *     Node<T>
      */
-    public Node(T value, Node<T> next)
+    public Node(T value, Node<T> left)
     {
         this.value = value;
-        this.next = next;
+        this.left = left;
+    }
+    // Begin Getters and Setters
+
+    /**
+     * @return Node<T>
+     */
+    public Node<T> getRight()
+    {
+        return right;
+    }
+
+    /**
+     * @param value
+     *     T
+     */
+    public Node<T> setRight(T value)
+    {
+        this.right = new Node<>(value);
+        return this;
     }
 
     /**
      * @return Node<T>
      */
-    public Node<T> getNext()
+    public Node<T> getleft()
     {
-        return this.next;
+        return left;
     }
 
     /**
-     * @param next
+     * Node<Integer> junk;
+     * junk is an instance of Node<T>.
+     * With fluent interface.
+     * junk->setValue(5)
+     * ->setLeft(1)
+     * ->setRight(10);
+     * <p>
+     * Without fluent interface.
+     * junk->setValue(5);
+     * junk->setLeft(1);
+     * junk->setRight(10);
+     *
+     * @param value
+     *     T
+     */
+    public Node<T> setLeft(T value)
+    {
+        this.left = new Node<>(value);
+        return this;
+    }
+
+    /**
+     *
+     * @param left
      *     Node<T>
      */
-    public void setNext(Node<T> next)
+    public Node<T> setLeft(Node<T> left)
     {
-        this.next = next;
+        this.left = left;
+        return this;
     }
-    // Begin Getters and Setters
+
+    /**
+     * @param right
+     *     Node<T>
+     */
+    public Node<T> setRight(Node<T> right)
+    {
+        this.right = right;
+        return this;
+
+    }
 
     /**
      * @return <T>
@@ -78,8 +164,9 @@ public class Node<T> {
      * @param value
      *     T
      */
-    public void setValue(T value)
+    public Node<T> setValue(T value)
     {
         this.value = value;
+        return this;
     }
 }
