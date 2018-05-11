2fc0c70ae24a89a695f7d37bda046dcaa8a779de
JJ <860024987@wwcc.edu>
2018-05-10 19:28:15 -0700

    Added demo for new Queue class methods(enqueue, dequeue, size, peek) to Main class.

--- src/edu/wwcc/CS_241/Jumper/Jonathan/Queue/Main.java
+++ src/edu/wwcc/CS_241/Jumper/Jonathan/Queue/Main.java
@@ -11,39 +11,39 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args)
     {
-        Queue<Integer> ll = new Queue<>();
-        System.out.println("\nLinkedList length = " + ll.length());
-        ll.print();
-        ll.append(111);
+        Queue<Integer> queue = new Queue<>();
+        System.out.println("\nLinkedList length = " + queue.length());
+        queue.print();
+        queue.append(111);
         System.out.println("* CREATED FIRST NODE *");
-        System.out.println("\nLinkedList length = " + ll.length());
-        ll.print();
+        System.out.println("\nLinkedList length = " + queue.length());
+        queue.print();
         System.out.println();
-        ll.append(222);
-        ll.append(333);
+        queue.append(222);
+        queue.append(333);
         System.out.println("* Finished appending and creating the LinkedList. 111,222,333 *");
-        System.out.println("\nLinkedList length = " + ll.length());
-        ll.print();
+        System.out.println("\nLinkedList length = " + queue.length());
+        queue.print();
         System.out.println();
         System.out.println("* Prepend Method (added 444 to beginning) *");
-        ll.prepend(444);
-        System.out.println("\nLinkedList length = " + ll.length());
-        ll.print();
+        queue.prepend(444);
+        System.out.println("\nLinkedList length = " + queue.length());
+        queue.print();
         System.out.println();
         System.out.println("* REMOVE AT INDEX 0 *");
-        ll.removeAt(0);
-        System.out.println("\nLinkedList length = " + ll.length());
-        ll.print();
+        queue.removeAt(0);
+        System.out.println("\nLinkedList length = " + queue.length());
+        queue.print();
         System.out.println();
         System.out.println("* REMOVE FIRST *");
-        ll.removeFirst();
-        System.out.println("\nLinkedList length = " + ll.length());
-        ll.print();
+        queue.removeFirst();
+        System.out.println("\nLinkedList length = " + queue.length());
+        queue.print();
         System.out.println();
         System.out.println("* REMOVE LAST *");
-        ll.removeLast();
-        System.out.println("\nLinkedList length = " + ll.length());
-        ll.print();
+        queue.removeLast();
+        System.out.println("\nLinkedList length = " + queue.length());
+        queue.print();
 
         /*
          * Random for insertAt() method
@@ -53,8 +53,8 @@ public class Main {
         int i = 0, num = 5;
         do
         {
-            ll.print();
-            System.out.println("LinkedList length = " + ll.length() + "\n");
+            queue.print();
+            System.out.println("LinkedList length = " + queue.length() + "\n");
             int value = r.nextInt(10);
             int position = r.nextInt(6);
             System.out.println("Inserting value: " + "*" + value * 111 + "*" + " at index: " + position);
@@ -63,7 +63,7 @@ public class Main {
              */
             try
             {
-                ll.insertAt(value * 111, position);
+                queue.insertAt(value * 111, position);
             } catch (IndexOutOfBoundsException e)
             {
                 //TODO: whatever. Exception is held with a try/catch
@@ -72,7 +72,19 @@ public class Main {
             }
             ++i;
         } while (i < num);
-        ll.print();
-        System.out.println("Final LinkedList length = " + ll.length());
+        System.out.println("\nQueueing method testing");
+        System.out.println("Enqueueing");
+        queue.print();
+        System.out.println("Final LinkedList length = " + queue.size());
+        queue.enqueue(101);
+        System.out.println("Dequeueing");
+        queue.print();
+        System.out.println("Final LinkedList length = " + queue.size());
+        queue.dequeue();
+        queue.print();
+        System.out.println("Final LinkedList length = " + queue.size());
+        queue.print();
+        System.out.println("Peeking at top value = " + queue.peek());
+        queue.print();
     }
 }
